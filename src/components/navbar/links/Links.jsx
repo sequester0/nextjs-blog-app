968802bed3055fee9b671df6935fc02874828ec4
İbrahim Kerem Gümüş <i.keremgumus@hotmail.com>
2024-01-26 18:32:46 +0300

    components and error, not-found, loading pages

---
+++ src/components/navbar/links/Links.jsx
@@ -0,0 +1,35 @@
+import Link from "next/link";
+
+const Links = () => {
+
+  const links = [
+    {
+        title: "Home",
+        path: "/"
+    },
+    {
+        title: "About",
+        path: "/about"
+    },
+    {
+        title: "Contact",
+        path: "/contact"
+    },
+    {
+        title: "Blog",
+        path: "/blog"
+    }
+  ];
+
+  return (
+    <div>
+        {
+            links.map((link) => (
+                <Link key={link.title} href={link.path}>{link.title}</Link>
+            ))
+        }
+    </div>
+  )
+}
+
+export default Links
